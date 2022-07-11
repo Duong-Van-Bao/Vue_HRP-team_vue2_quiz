ed73c3ad2c9ff4839bbf2d18326537e4bcad2acb
Duong_Van_Bao <duongbao9109@gmail.com>
2022-07-11 18:31:58 +0700

    project quiz

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+  <b-nav tabs>
+    <b-nav-item disabled>Fancy Quiz App</b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+</div>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template #lead>
+        Some question here ?
+      </template>
+      <hr class="my-4">
+      <b-button
+        variant="primary"
+        href="#"
+      >Submit</b-button>
+      <b-button
+        variant="success"
+        href="#"
+      >Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
